dac2b5c3f26cc1da16161681dbe1b94e4f83404a
Leon1990 <258849433@qq.com>
2014-12-07 09:25:14 +0800

    20141207

---
+++ test_canoe.c
@@ -0,0 +1,20 @@
+#include "vxworks.h"
+#include "devicenet.h"
+
+int usrAppStartProc(void)
+{
+    struct CAN_RPACKET rcvePacket;
+    while
+    {
+        CanRcvMsg(0,rcvePacket);
+        CanRcvMsg(1,rcvePacket);
+        CanRcvMsg(2,rcvePacket);
+        CanRcvMsg(3,rcvePacket);
+    }
+}
+int usrAppConfig(void)
+{   
+    CanInit(3,4);
+    printf("ok!\n");
+}
+
